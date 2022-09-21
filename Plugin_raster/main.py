75a8feb7221161aa50871613fec497fee0228654
Julio Cesar Vences Serrato <jvencess001@alumno.uaemex.mx>
2022-09-21 08:30:09 -0500

    plugin_raster

--- Plugin_raster/main.py
+++ Plugin_raster/main.py
@@ -29,3 +29,30 @@ class mainMenu:
     def startInterfaz(self):
         self.dialogo = interfaz()
         self.dialogo.show()
+        layers = QgsProject.instance().mapLayers().values() 
+        for layer in layers:
+            if layer.type() == QgsMapLayer.VectorLayer and layer.geometryType()== QgsWkbTypes.PolygonGeometry:
+                vLayer = layer
+            if layer.type() == QgsRasterLayer.RasterLayer:
+                rLayer = layer
+                self.dialogo.ui.cmb1.addItem(rLayer.name())
+                epsg = rLayer.crs().authid()
+                self.dialogo.ui.lb1.setText(str(epsg))
+                ext = rLayer.extent()
+                alto = rLayer.height()
+                pixelX = rLayer.rasterUnitsPerPixelX()
+                pixelY = rLayer.rasterUnitsPerPixelY()
+                xmin = ext.xMinimum()
+                xmax = ext.xMaximum()
+                ymin = ext.yMinimum()
+                ymax = ext.yMaximum()
+                self.dialogo.ui.txEdt.setText('Coordenadas: \n' + str(ext) +
+                                              '\nAltura: \n' + str(alto) + 
+                                              '\nTamaño pixel X: \n' + str(pixelX) +
+                                              '\nTamaño pixel Y: \n' + str(pixelY) +
+                                              '\nX Min: \n' + str(xmin) +
+                                              '\nX Max: \n' + str(xmax) + 
+                                              '\nY Min: \n' + str(ymin) +
+                                              '\nY Max: \n' + str(ymax))
+    def unload(self):
+            QgsApplication.processingRegistry().removeProvider(self.provider)
